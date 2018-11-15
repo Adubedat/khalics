475beb80981c764a32b74c77d8bec6db9c7f68b4
Renan PORCON <rporcon@student.42.fr>
2018-11-15 10:39:51 +0100

    eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,20 @@
 module.exports = {
 	"extends": "airbnb",
-    "parser": "babel-eslint",
-    "env": {
-      "browser": true,
-      "node": true,
-      "es6": true,
-      "mocha": true
-    },
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true,
+    "mocha": true
+  },
 	"rules": {
   	"react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
-	}
+	},
+	"plugins": [
+    "react",
+    "react-native"
+  ],
+	"ecmaFeatures": {
+    "jsx": true
+  }
 };
