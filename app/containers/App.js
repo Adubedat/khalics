026942e6ee892b95643a2148eb531ca959671ad1
Renan PORCON <rporcon@student.42.fr>
2018-11-15 15:03:11 +0100

    details

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,32 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 import * as Expo from 'expo';
 import SignUp from './SignUp';
 import SignIn from './SignIn/SignIn';
 
-
-// export default class App extends React.PureComponent {
-//   render() {
-//     return (
-//       <SignIn />
-//     );
-//   }
-// }
-
 const App = createStackNavigator({
   SignIn: { screen: SignIn },
   SignUp: { screen: SignUp },
-});
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
 
 Expo.registerRootComponent(App);
