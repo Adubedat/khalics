8bdbe70b5f0c6ff411325853cfd71b19ab869c80
Renan PORCON <rporcon@student.42.fr>
2018-11-30 17:27:03 +0100

    test getWorkout request

--- app/containers/WorkoutList/WorkoutList.js
+++ app/containers/WorkoutList/WorkoutList.js
@@ -5,35 +5,32 @@ import StepIndicator from '../../components/StepIndicator';
 import { styles, stepIndicatorStyles } from './styles';
 import TestMySkills from '../testMySkills/testMySkills';
 
-// TODO: fetch workouts from DB or from generate workout
-const workouts = [
-  {
-    name: 'Full body',
-    description: 'dips, pullup, squat',
-  },
-  {
-    name: 'Upper body',
-    description: 'push up, chin up, l-sit',
-  },
-  {
-    name: 'Lower body',
-    description: 'squat, deadlift, calves raise',
-  },
-];
-
 export default class WorkoutList extends Component {
   constructor() {
     super();
 
+    this.test = true; // to delete
     this.state = {
       currentWorkout: 0,
-      testSkill: true,
+      workouts: [],
     };
   }
 
+  async componentDidMount() {
+    const getWorkoutUrl = 'https://qmzsdq8495.execute-api.eu-west-1.amazonaws.com/dev/workout/get';
+    if (this.test) {
+      // below is example normally get workouts ids from user data
+      const res = await fetch(`${getWorkoutUrl}?ids=["test", "test2"]`);
+      const workouts = await res.json();
+      const state = { ...this.state, workouts };
+      this.setState(state);
+    }
+  }
+
   render() {
-    const { currentWorkout, testSkill } = this.state;
-    if (testSkill) {
+    const { currentWorkout, workouts } = this.state;
+    console.log('workouts', workouts);
+    if (workouts.length === 0) {
       return (
         <TestMySkills />
       );
--- app/containers/testMySkills/testMySkills.js
+++ app/containers/testMySkills/testMySkills.js
@@ -19,34 +19,23 @@ export default class TestMySkills extends Component {
           <Text h2 style={styles.mainTitle}>
             Test my skills
           </Text>
-          <Text h4 style={styles.presentationTxt}>
+          <Text style={styles.presentationTxt}>
             In Khalics we will generate a workout that fit you.
             {'\n\n'}
             We use powerful algorithm to reach this goal.
             {'\n\n'}
-            We first need to test you, then we will be able to evaluate your skill
+            First, we need to test you, then we will be able to evaluate your skill
             and generate for you a workout.
             {'\n\n'}
             The aim of the workout is to improve your skill towards mastering
             calisthenics figures.
-            {/* {'\n\n'}
-          Morbi ut urna venenatis, dictum tortor eget, consequat ante.
-          Suspendisse id convallis mauris, in fermentum urna. Etiam at ornare orci.
-          Praesent arcu massa, eleifend quis turpis in, auctor consequat lectus.
-          Etiam ornare, massa vitae iaculis mattis, mi velit porta libero, et egestas tellus ex cursus sem.
-          Nam vel pulvinar eros, aliquet aliquam nunc.
-            {'\n\n'}
-          Ut vel tellus elementum, efficitur felis quis, finibus arcu.
-          Maecenas efficitur nulla urna, fermentum vehicula dolor ornare at.
-          Duis posuere nisl tincidunt tortor venenatis, non venenatis dolor porta.
-          Quisque sit amet suscipit nisi */}
           </Text>
           <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
             <Button
               raised
               containerStyle={{ flex: 0.8 }}
-              buttonStyle={{ backgroundColor: 'white' }}
-              titleStyle={{ color: theme.darkGray3, fontWeight: 'bold', fontSize: 25 }}
+              buttonStyle={{ backgroundColor: theme.red }}
+              titleStyle={{ color: 'white', fontWeight: 'bold', fontSize: 25 }}
               title="Test my skills"
             />
           </View>
