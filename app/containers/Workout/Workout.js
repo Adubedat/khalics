44f253778c9f5f831ba7e4587ba1585f94a9de24
Renan PORCON <rporcon@student.42.fr>
2018-12-04 16:04:36 +0100

    fetch /exercise/get workout ok

--- app/containers/Workout/Workout.js
+++ app/containers/Workout/Workout.js
@@ -5,8 +5,25 @@ class Workout extends React.PureComponent {
   constructor(props) {
     super(props);
     const { navigation } = this.props;
-    console.log(navigation.state.params);
     this.state = {};
+  }
+
+  async componentDidMount() {
+    const getExerciseUrl = 'https://qmzsdq8495.execute-api.eu-west-1.amazonaws.com/dev/exercise/get';
+    // below is example normally get workouts ids from user data
+
+    const { navigation } = this.props;
+    const { exercises } = navigation.state.params.workout;
+    const ids = exercises.reduce((acc, val, index, array) => {
+      let param = `"${val.id}"`;
+      if (index !== array.length - 1) { param += ', '; }
+      return acc + param;
+    }, '');
+    const res = await fetch(`${getExerciseUrl}?ids=[${ids}]`);
+    const resJson = await res.json();
+    console.log(resJson);
+    // const state = { ...this.state, workouts: resJson.workouts, loading: false };
+    // this.setState(state);
   }
 
   render() {
