3e2904e7f1ebdda180e4f4084c3cf09e8e109920
Arthur Dubedat <arthur.dubedat@gmail.com>
2018-12-11 17:32:17 +0100

    renderTimer function done

--- app/containers/OnGoingWorkout/OnGoingWorkout.js
+++ app/containers/OnGoingWorkout/OnGoingWorkout.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View, Text, ScrollView, Dimensions,
+} from 'react-native';
 import { Button } from 'react-native-elements';
+import ProgressCircle from 'react-native-progress/Circle';
 import theme from '../../theme';
 import styles from './styles';
 
@@ -14,14 +17,30 @@ class OnGoingWorkout extends React.Component {
     const { navigation } = this.props;
     this.workout = navigation.getParam('workout');
     this.exercises = navigation.getParam('exercises');
+    this.timeoutId = null;
     this.state = {
       currentExerciseIndex: 0,
       currentSetIndex: 1,
       totalSetDone: 0,
+      restTime: 0,
     };
     const { currentExerciseIndex } = this.state;
     navigation.setParams({ title: this.exercises[currentExerciseIndex].name });
     console.log(this.props.navigation.state.params);
+  }
+
+  componentDidUpdate() {
+    const { restTime } = this.state;
+    if (!this.timeoutId && restTime > 0) {
+      this.timeoutId = setInterval(
+        () => this.setState(prevState => ({ restTime: prevState.restTime - 1 })),
+        1000,
+      );
+    }
+    if (restTime <= 0) {
+      clearInterval(this.timeoutId);
+      this.timeoutId = null;
+    }
   }
 
   nextSet = () => {
@@ -36,6 +55,7 @@ class OnGoingWorkout extends React.Component {
           currentExerciseIndex: i,
           currentSetIndex: temp + 1,
           totalSetDone: totalSetDone + 1,
+          restTime: workout.restTime,
         });
         navigation.setParams({ title: exercises[i].name });
         return;
@@ -44,23 +64,48 @@ class OnGoingWorkout extends React.Component {
     }
   }
 
+  renderTimer = () => {
+    const screenWidth = Dimensions.get('window').width;
+    const fontSize = screenWidth / 16;
+    return (
+      <View style={{
+        flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around',
+      }}
+      >
+        <View style={{ width: screenWidth / 3, paddingRight: 20 }}>
+          <Text style={[styles.header_text, { fontSize, textAlign: 'right' }]}>Rest</Text>
+        </View>
+        <ProgressCircle
+          progress={restTime / this.workout.restTime}
+          color={theme.red}
+          size={screenWidth / 4}
+          borderWidth={0}
+          unfilledColor={theme.darkGray1}
+          showsText
+          formatText={() => restTime}
+          textStyle={{ color: 'white', fontSize }}
+        />
+        <View style={{ width: screenWidth / 3, paddingLeft: 20 }}>
+          <Text style={[styles.header_text, { fontSize, textAlign: 'left' }]}>seconds</Text>
+        </View>
+      </View>
+    );
+  }
+
   render() {
-    const { currentExerciseIndex, currentSetIndex } = this.state;
+    const { currentExerciseIndex, currentSetIndex, restTime } = this.state;
     console.log(this.state);
     return (
       <View style={styles.container}>
-        <View style={{ flex: 1 }}>
-          <Text>Rest container</Text>
+        <View style={styles.header_container} />
+        <View style={styles.exercise_container}>
+          <ScrollView>
+            <Text>Exercise component</Text>
+          </ScrollView>
         </View>
-        <View style={{ flex: 1 }}>
-          <Text>Exercise component</Text>
-        </View>
-        <View style={{
-          flex: 1, justifyContent: 'center', backgroundColor: theme.darkGray2, padding: 16,
-        }}
-        >
+        <View style={styles.button_container}>
           <Button
-            title="START"
+            title="DONE"
             raised
             containerStyle={{ borderRadius: 2, backgroundColor: theme.darkGray2 }}
             buttonStyle={{ backgroundColor: theme.red }}
--- app/containers/OnGoingWorkout/styles.js
+++ app/containers/OnGoingWorkout/styles.js
@@ -8,7 +8,22 @@ export default StyleSheet.create({
     justifyContent: 'space-around',
   },
   header_container: {
-    flex: 1,
+    flex: 3,
     backgroundColor: theme.darkGray3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header_text: {
+    color: 'white',
+    fontWeight: '500',
+  },
+  exercise_container: {
+    flexGrow: 7,
+  },
+  button_container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: theme.darkGray2,
+    padding: 16,
   },
 });
